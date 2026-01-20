697d6d9ba6f8a063f0d6f6af23f54071eeaebec3
연구개발전담부 <wdpb@mig.kr>
2026-01-20 17:21:49 +0900

    fix

---
+++ next-sitemap.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.miraeinro.com/",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "weekly",
+  priority: 0.7,
+
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+  },
+
+  exclude: ["/admin", "/admin/*"],
+};
